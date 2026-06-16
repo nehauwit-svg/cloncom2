8f316759d08f68909439dd6a78758d1cc071da21
nehauwit-svg <nehauwit@gmail.com>
2026-06-16 15:39:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "frontendnode-production.up.railway.app";
+  const shopifyDomain = "careersprint.7f.liveblog365.com";
   const proxyHost = req.headers.host;
 
   const targetURL = `https://${shopifyDomain}${req.url}`;
